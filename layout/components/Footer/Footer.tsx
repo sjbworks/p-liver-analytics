e7455fd882b1be7f5c003cc8bba662451ddf745a
sjbworks <sacred.jizo.bosatz@gmail.com>
2021-04-20 00:10:09 +0900

    style: edit footer style

--- layout/components/Footer/Footer.tsx
+++ layout/components/Footer/Footer.tsx
@@ -6,22 +6,22 @@ import faceManShimmer from '@iconify/icons-mdi/face-man-shimmer'
 import { Text } from '../../../components/atoms/Text'
 
 export const Footer = () => (
-  <footer className="flex bg-blue-600">
+  <footer className="flex h-16 bg-blue-600">
     <Link href="/corefan">
-      <div>
-        <Icon icon={speedometerIcon} color="white" />
+      <div className="flex items-center justify-center flex-1">
+        <Icon icon={speedometerIcon} color="white" width="32" />
         {/* <Text>Meter</Text> */}
       </div>
     </Link>
     <Link href="/meter">
-      <div>
-        <Icon icon={faceManShimmer} color="white" />
+      <div className="flex items-center justify-center flex-1">
+        <Icon icon={faceManShimmer} color="white" width="32" />
         {/* <Text>CoreFan</Text> */}
       </div>
     </Link>
     <Link href="/register">
-      <div>
-        <Icon icon={fileEdit} color="white" />
+      <div className="flex items-center justify-center flex-1">
+        <Icon icon={fileEdit} color="white" width="32" />
         {/* <Text>Register</Text> */}
       </div>
     </Link>
