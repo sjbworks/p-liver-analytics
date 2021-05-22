b2d92a67dd9cecb38f0469be659ed7a1d04c5f2e
sjbworks <sacred.jizo.bosatz@gmail.com>
2021-05-23 01:08:02 +0900

    style: edit the LineChart styles

--- components/molecules/LineChart/LineChart.tsx
+++ components/molecules/LineChart/LineChart.tsx
@@ -1,9 +1,15 @@
 import dynamic from 'next/dynamic'
 import React from 'react'
 import { generateOption, LineChartProps } from '.'
+import { Card } from 'components/atoms/Card'
+
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
 
 export const LineChart = ({ series, categories }: LineChartProps<string>) => {
   const options = generateOption(categories)
-  return <Chart options={options} series={series} type="line" />
+  return (
+    <Card className="p-5">
+      <Chart options={options} series={series} type="line" />
+    </Card>
+  )
 }
